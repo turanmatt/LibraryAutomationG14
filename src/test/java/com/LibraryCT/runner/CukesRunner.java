79fb8d1f89275628f9228f8b36823e1291d299e8
turanMert <t.meert.k@gmail.com>
2022-11-28 13:51:57 -0500

    Runners updated

--- src/test/java/com/LibraryCT/runner/CukesRunner.java
+++ src/test/java/com/LibraryCT/runner/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features" ,
-        glue = "com/cydeo/step_Defs" ,
+        glue = "com/LibraryCT/step_Defs" ,
         dryRun = false,
         tags = "@smoke"
 )
--- src/test/java/com/LibraryCT/runner/FailedTestRunner.java
+++ src/test/java/com/LibraryCT/runner/FailedTestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "@target/rerun.txt",
-        glue = "com/cydeo/step_definitions"
+        glue = "com/LibraryCT/step_Defs"
 )
 public class FailedTestRunner {
 }
