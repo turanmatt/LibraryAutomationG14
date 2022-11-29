999382d2402ab6cdbf5c389ac9cbcb74e00d3265
Lauren Przywara <alicenoble.py@gmail.com>
2022-11-28 20:59:07 -0500

    Began working on step defs for Scenario 1.

--- src/test/java/com/LibraryCT/stepDefs/US1_1_UserInformationTable_StepDef.java
+++ src/test/java/com/LibraryCT/stepDefs/US1_1_UserInformationTable_StepDef.java
@@ -1,24 +1,31 @@
 package com.LibraryCT.stepDefs;
 
+import com.LibraryCT.utilities.ConfigurationReader;
+import com.LibraryCT.utilities.DB_Util;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.sql.ResultSet;
+
 public class US1_1_UserInformationTable_StepDef {
+    int actualUserIDCount;
+    DB_Util db=new DB_Util();
+
     @Given("Establish the database connection")
     public void establish_the_database_connection() {
-
-        throw new io.cucumber.java.PendingException();
+        DB_Util.createConnection(ConfigurationReader.getProperty("library2.db.url"), ConfigurationReader.getProperty("library2.db.username"), ConfigurationReader.getProperty("library2.db.password"));
     }
     @When("Execute query to get user IDs")
     public void execute_query_to_get_user_i_ds() {
+        //TODO: Get a count of all Users
 
-        throw new io.cucumber.java.PendingException();
+
+DB_Util.runQuery("select COUNT(id) from users");
     }
     @Then("verify all users have unique ID")
     public void verify_all_users_have_unique_id() {
-
-        throw new io.cucumber.java.PendingException();
+//TODO: Create a set of all user ids. Compare count of set with number of users to determine if ids are unique.
     }
 
 
@@ -30,4 +37,6 @@ public class US1_1_UserInformationTable_StepDef {
     @Then("verify the below <{string}> are listed in result")
     public void verifyTheBelowAreListedInResult(String arg0) {
     }
+
+
 }
