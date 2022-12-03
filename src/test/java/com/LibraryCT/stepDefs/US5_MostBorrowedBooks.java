760716d76b2c2b8ab117320eea6c0d51269bc256
turanMert <t.meert.k@gmail.com>
2022-12-02 23:15:48 -0500

    Feature File and Step Def Created

---
+++ src/test/java/com/LibraryCT/stepDefs/US5_MostBorrowedBooks.java
@@ -0,0 +1,24 @@
+package com.LibraryCT.stepDefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US5_MostBorrowedBooks {
+
+
+    @Given("Establish the database connection")
+    public void establish_the_database_connection() {
+
+    }
+    @When("I execute a query to find the most popular book genre")
+    public void i_execute_a_query_to_find_the_most_popular_book_genre() {
+
+    }
+    @Then("verify {string} is the most popular book genre.")
+    public void verify_is_the_most_popular_book_genre(String string) {
+
+    }
+
+
+}
---
+++ src/test/resources/features/US5_MostBorrowedBooks.feature
@@ -0,0 +1,7 @@
+
+@wip
+Feature:As a librarian, I want to know what genre of books is being borrowed the most
+  Scenario: Verify the common book genre that’s being borrowed
+    Given Establish the database connection
+    When I execute a query to find the most popular book genre
+    Then verify "Action and Adventure" is the most popular book genre.
