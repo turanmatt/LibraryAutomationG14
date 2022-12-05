9b3b82367b36319f39d6d6107e44b114a91757aa
Zeynep Ozler <zynpzlr@gmail.com>
2022-12-04 19:26:14 -0500

    US2.1 updated.

--- src/test/java/com/LibraryCT/stepDefs/US2_1_Borrowed_Books_Number_StepDefs.java
+++ src/test/java/com/LibraryCT/stepDefs/US2_1_Borrowed_Books_Number_StepDefs.java
@@ -10,13 +10,13 @@ public class US2_1_Borrowed_Books_Number_StepDefs {
     CommonAreaPage commonAreaPage = new CommonAreaPage();
     String actualBorrowedBooks;
 
-    @When("user take borrowed books number")
-    public void user_take_borrowed_books_number() {
+    @When("I take borrowed books number")
+    public void I_take_borrowed_books_number() {
         actualBorrowedBooks = commonAreaPage.borrowedBooksNumber.getText();
     }
 
-    @Then("borrowed books number information must match with DB")
-    public void borrowed_books_number_information_must_match_with_db() {
+    @Then("the borrowed books number information must match with DB")
+    public void the_borrowed_books_number_information_must_match_with_db() {
 
         String query = "select count(*) from book_borrow\n" +
                 "where is_returned=0";
